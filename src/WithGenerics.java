26321455f83dfdcb7776a516b3e859b8e495ce62
utdiscas <utdiscas@protonmail.com>
2018-06-19 01:24:48 +0200

    function with generic return type

--- src/WithGenerics.java
+++ src/WithGenerics.java
@@ -9,12 +9,24 @@ public class WithGenerics {
 
         printMe(iray);
         printMe(cray);
+
+        System.out.println(max(123,23,32));
+       System.out.println(max("equation","fluid:", "epsilon"));
     }
 
     public static <T> void  printMe(T[] x){
         for(T b: x)
             System.out.printf("%s ", b);
         System.out.println();
+    }
+//maximum of variables
+    public static <T extends Comparable<T>> T  max(T a, T b, T c){
+        T m=a;
+        if(b.compareTo(a) > 0)
+            m=b;
+        if(c.compareTo(m) >0 )
+            m=c;
+        return m;
     }
 
 
